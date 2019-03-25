bd95aeca682455b07595534d3987b4c5f83a66ee
Roney Gomes <roney477@gmail.com>
2019-03-25 10:32:48 -0300

    remove backend config

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">= 0.11.12"
-
-  backend "s3" {
-    bucket = "tf-remote-state-hatchdata-iam"
-    workspace_key_prefix = "sample-terraform-project"
-    key = "terraform.tfstate"
-    region ="us-west-2"
-  }
 }
 
 provider "aws" {
